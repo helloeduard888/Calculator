043e49fcfa0cb0974e16d47633b5886f77113714
Fahrenheit <igor.maisyk@gmail.com>
2022-11-10 17:18:26 +0300

    Show bets history

--- BetCalculator/Screens/History/HistoryView.swift
+++ BetCalculator/Screens/History/HistoryView.swift
@@ -8,13 +8,57 @@
 import SwiftUI
 
 struct HistoryView: View {
+    var format: (Double) -> String
+    
     var body: some View {
-        Text("History")
+        BetsView(viewModel: .init(persistence: .shared,
+                                  predicate: NSPredicate(format: "isWon != NULL")),
+                 viewForBet: view(for:))
+        .onMainBackground()
+    }
+    
+    private func view(for bet: Bet) -> some View {
+        VStack {
+            HStack(spacing: 30) {
+                VStack(alignment: .leading, spacing: 10) {
+                    Text("Bet:")
+                        .font(.mainFont(size: 15, weight: .light))
+                    Text("Amount:")
+                        .font(.mainFont(size: 15, weight: .light))
+                    Text("Multiplier:")
+                        .font(.mainFont(size: 15, weight: .light))
+                    Text(bet.isWon == true ? "Won:" : "Lost")
+                        .foregroundColor(bet.isWon == true ? .green : .red)
+                        .font(.mainFont(size: 15, weight: .light))
+                }
+                .foregroundColor(.white)
+                
+                Spacer()
+                
+                VStack(alignment: .leading, spacing: 8) {
+                    Text(bet.title ?? "N/A")
+                        .font(.mainFont(weight: .semibold))
+                    Text(format(bet.amount))
+                        .font(.mainFont(weight: .semibold))
+                    Text(String(format: "%.2f", bet.multiplier))
+                        .font(.mainFont(weight: .semibold))
+                    Text(bet.isWon == true ? format(bet.amount * bet.multiplier) : "")
+                        .font(.mainFont(weight: .semibold))
+                        .foregroundColor(.green)
+                }
+                .foregroundColor(.white)
+                
+            }
+            
+            Rectangle().stroke(.white).frame(maxHeight: 0.5)
+        }
+        .padding(.horizontal)
+        .padding(.top)
     }
 }
 
 struct HistoryView_Previews: PreviewProvider {
     static var previews: some View {
-        HistoryView()
+        HistoryView(format: { i in String(format: "%.1f", i) + Constants.currency})
     }
 }
--- BetCalculator/Screens/Tab/MainTabView.swift
+++ BetCalculator/Screens/Tab/MainTabView.swift
@@ -9,21 +9,24 @@ import SwiftUI
 
 struct MainTabView: View {
     @State private var selection = 0
+    @StateObject var mainModdel = MainViewViewModel(persistence: .shared)
     
     var body: some View {
         CustomTabView(selection: $selection) { index in
             switch index {
             case 0:
-                MainView(viewModel: .init(persistence: .shared))
+                MainView(viewModel: mainModdel)
                     .transition(.identity)
             case 1:
                 AddView(viewModel: .init(persistence: .shared)) {
                     withAnimation { selection = 0 }
                 }
-                    .transition(.identity)
+                .transition(.identity)
             case 2:
-                HistoryView()
-                    .transition(.identity)
+                HistoryView { amount in
+                    String(format: "%.1f", amount) + Constants.currency
+                }
+                .transition(.identity)
             default:
                 EmptyView()
             }
